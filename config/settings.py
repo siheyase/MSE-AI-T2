f3e04a17ef8a3a9197f51b5c966bc38b03321610
Linshou99 <90241934+Linshou99@users.noreply.github.com>
2025-06-16 20:57:29 +0800

    Create settings.py

---
+++ config/settings.py
@@ -0,0 +1,6 @@
+DEFAULT_MODEL = "qwen2.5:14b-instruct-fp16"
+VS_PATH = "/home/qinwen/medRAG/vs"
+VS_DOCS_PATH = "/home/qinwen/medRAG/docs"
+EMBEDDING_MODEL = "bge-m3"
+SEARCH_TYPE = "similarity"
+TOP_K = 2
