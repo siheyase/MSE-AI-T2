825bde4404b9723aecc47511dbe511915377b520
siheyase <xuqi18268712006@163.com>
2025-06-23 12:15:48 +0000

    fix

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,14 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__)) 
+
 DEFAULT_MODEL = "qwen2.5:14b-instruct-fp16"
-VS_PATH = "/home/qinwen/medRAG/vs"
-VS_DOCS_PATH = "/home/qinwen/medRAG/docs"
 EMBEDDING_MODEL = "bge-m3"
-SEARCH_TYPE = "similarity"
+
+ROOT_DIR = os.path.abspath(os.path.join(BASE_DIR, ".."))  
+
+VS_PATH = os.path.join(ROOT_DIR, "vs")
+VS_DOCS_PATH = os.path.join(ROOT_DIR, "docs")
+
 TOP_K = 2
+SEARCH_TYPE = "similarity"
