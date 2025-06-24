8f832892d9acf068bf2a5703c2dc4653df8484d8
siheyase <xuqi18268712006@163.com>
2025-06-24 04:43:33 +0000

    fix

--- config/settings.py
+++ config/settings.py
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-DEFAULT_MODEL = "qwen2.5:14b-instruct-fp16"
-VS_PATH = "/root/MSE/medRAG/vs"
-VS_DOCS_PATH = "/root/MSE/medRAG/docs"
-EMBEDDING_MODEL = "bge-m3"
-SEARCH_TYPE = "similarity"
-TOP_K = 2
-=======
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__)) 
@@ -20,4 +12,3 @@ VS_DOCS_PATH = os.path.join(ROOT_DIR, "docs")
 
 TOP_K = 2
 SEARCH_TYPE = "similarity"
->>>>>>> origin/main
